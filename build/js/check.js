b351d73d35d2cacc3490d1a33247c1f1cfde8309
Alexey <azamatdj@gmail.com>
2016-08-05 12:27:31 +0400

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,25 +1,23 @@
 function getMessage (a, b) {
   if(typeof(a) === "boolean"){
-    if (a === true) {
+    if (a) {
       return "Я попал в " + b;
     } else {
       return "Я никуда не попал";
     }
-  }
-
-  if (typeof(a) === 'number') {
+  } else if (typeof(a) === 'number') {
     return "Я прыгнул на " + (a * 100) + " сантиметров";
-  }
+  } else {
 
-  if (a instanceof Array && b instanceof Array) {
-    var distancePath = 0;
-    for (var i = 0; i < a.length; i++) {
-      distancePath += a[i] * b[i];
-    }
-    return "Я прошёл " + distancePath + " метров";
-  }
+	  if (a instanceof Array && b instanceof Array) {
+	    var distancePath = 0;
+	    var minLength = Math.min(a.length, b.length);
+	    for (var i = 0; i < minLength; i++) {
+	      distancePath += a[i] * b[i];
+	    }
+	    return "Я прошёл " + distancePath + " метров";
+	  }
 
-  if (a instanceof Array) {
     var numberOfSteps = 0;
     for (var i = 0; i < a.length; i++) {
       numberOfSteps += a[i];
