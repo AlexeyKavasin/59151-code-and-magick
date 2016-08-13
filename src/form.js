810a5c0b26ea7ff4b17ac9861a3c9c69e6afc236
akavasin <azamatdj@gmail.com>
2016-08-13 22:56:04 +0400

    Form validation

--- src/form.js
+++ src/form.js
@@ -1,6 +1,48 @@
 'use strict';
 
 window.form = (function() {
+  var reviewMark = document.querySelectorAll('input[name="review-mark"]');
+  var reviewName = document.querySelector('#review-name');
+  var reviewLabelName = document.querySelector('.review-fields-name');
+  var reviewText = document.querySelector('#review-text');
+  var reviewLabelText = document.querySelector('.review-fields-text');
+  var submit = document.querySelector('.review-submit');
+  var reviewFieldsBlock = document.querySelector('.review-fields');
+
+  reviewName.required = true;
+  // reviewMark.value = 1;
+  // reviewName.value = 'Aleksey';
+  // reviewText.value = 'Игра норм!';
+  for (var i = 0; i < reviewMark.length; i++) {
+    reviewMark[i].onchange = disableSubmit();
+  }
+  reviewName.oninput = disableSubmit();
+  reviewText.oninput = disableSubmit();
+
+  function disableSubmit() {
+    console.log(12);
+    reviewText.required = reviewMark.value < 3;
+    submit.disabled = !(reviewText.validity.valid && reviewName.validity.valid);
+
+    if(reviewText.validity.valid) {
+      reviewLabelText.style.visibility = 'hidden';
+    } else {
+      reviewLabelText.style.visibility = 'visible';
+    }
+
+    if(reviewName.validity.valid) {
+      reviewLabelName.style.visibility = 'hidden';
+    } else {
+      reviewLabelName.style.visibility = 'visible';
+    }
+
+    if(reviewName.validity.valid && reviewText.validity.valid) {
+      reviewFieldsBlock.style.visibility = 'hidden';
+    } else {
+      reviewFieldsBlock.style.visibility = 'visible';
+    }
+  }
+
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
 
@@ -24,11 +66,11 @@ window.form = (function() {
     }
   };
 
-
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     form.close();
   };
 
   return form;
+
 })();
