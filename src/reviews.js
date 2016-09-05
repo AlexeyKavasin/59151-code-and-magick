4fb914d9e5a759ee68396015ae8ff180297a7a37
akavasin <azamatdj@gmail.com>
2016-09-05 21:22:49 +0400

    Review constructor and review-quiz-answer selector added

--- src/reviews.js
+++ src/reviews.js
@@ -2,7 +2,7 @@
 
 var reviewsContainer = document.querySelector('.reviews-list');
 var reviewsFilter = document.querySelector('.reviews-filter');
-var getReviewElement = require('./review');
+var Review = require('./review');
 var load = require('./load');
 
 var reviewsVisibilityToggle = function() {
@@ -13,8 +13,9 @@ var addReviews = function() {
   reviewsVisibilityToggle();
 
   load('http://localhost:1506/api/reviews?callback=', function() {
-    window.reviews.forEach(function(review) {
-      getReviewElement(review, reviewsContainer);
+    window.reviews.forEach(function(item) {
+      var review = new Review(item);
+      reviewsContainer.appendChild(review.element);
     });
   });
 
